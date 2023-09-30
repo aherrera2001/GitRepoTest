55fb342852bc1bc2dcdb2c4ace59c6522ff2edfa
aherrera2001 <arelyherrera.075@gmail.com>
2023-09-30 12:21:25 -0400

    Initial Commit

---
+++ GitRepoTest/GitRepoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitRepoTestApp.swift
+//  GitRepoTest
+//
+//  Created by Arely Herrera on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct GitRepoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
